bac65edf14db131aeaf4f23619f55d39c87d9b1c
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2023-04-17 10:56:57 +0200

    recreate repository

---
+++ src/test/resources/import.sql
@@ -0,0 +1,21 @@
+
+INSERT INTO "db_photographer.pics" ("description", "title", "url", "visible") VALUES('couple with a sunset', 'marriage', 'https://i.pinimg.com/originals/d9/f6/7f/d9f67f261471c700ea9765f61ddc979e.jpg', 1);
+INSERT INTO "db_photographer.pics" ("description", "title", "url", "visible") VALUES('real Java spotted', 'Java', 'https://italy.armymwr.com/application/files/5616/1461/3153/vz_bod_java_coffee_beans_scoop_750x421_mar21.jpg', 1);
+INSERT INTO "db_photographer.pics" ("description", "title", "url", "visible") VALUES('', 'Woody\'s selfie', 'https://www.corriere.it/methode_image/2018/05/24/Tecnologia/Foto%20Tecnologia%20-%20Trattate/selfie-woody-toy-story-k6ZF-U43490714368647cI-593x443@Corriere-Web-Sezioni.jpg', 0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
